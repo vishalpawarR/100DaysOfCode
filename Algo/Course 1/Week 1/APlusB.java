a604721ba05731901b100bd5da913eb1e2411814
Vishal Pawar <55725885+vishalpawarR@users.noreply.github.com>
2020-11-06 20:40:46 +0530

    APlusB

--- Algo/Course 1/Week 1/APlusB.java
+++ Algo/Course 1/Week 1/APlusB.java
@@ -1,1 +1,14 @@
+import java.util.Scanner;
 
+class APlusB {
+    static int sumOfTwoDigits(int a, int b) {
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        System.out.println(sumOfTwoDigits(a, b));
+    }
+}
