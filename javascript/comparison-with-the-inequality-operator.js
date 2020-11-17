caaca93f173b7dc723f1563960df58bbfa22f5a0
Vishal Pawar <55725885+vishalpawarR@users.noreply.github.com>
2020-11-17 13:45:15 +0530

    != inequality op js

---
+++ javascript/comparison-with-the-inequality-operator.js
@@ -0,0 +1,14 @@
+// Setup
+function testNotEqual(val) {
+  if (val!=99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testNotEqual(10);
+testNotEqual(99);
+testNotEqual("99");
+testNotEqual(12);
+testNotEqual("12");
+testNotEqual("bob");
