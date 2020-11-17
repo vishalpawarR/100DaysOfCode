7ca493268354dadfa8719206ddd7de9b175bce8b
Vishal Pawar <55725885+vishalpawarR@users.noreply.github.com>
2020-11-17 16:54:46 +0530

    storing object in arrays

---
+++ javascript/manipulating-complex-objects.js
@@ -0,0 +1,24 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add a record here
+  {
+    "artist":"Score",
+    "title":"Stronger",
+    "release_year": 2019,
+    "formats": [
+      "CD",
+      "BT",
+      "LP"
+    ],
+  }
+];
